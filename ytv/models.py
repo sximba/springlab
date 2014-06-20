66ebcb49540dc9288819100847111efcbae60a52
Sandile <lungeloximba@gmail.com>
2014-06-20 22:58:00 +0200

    Created model for videos
    -- Video model created

--- ytv/models.py
+++ ytv/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    url = models.CharField(max_length = 1000)
+    ytid = models.CharField('YouTube ID', max_length = 11)
+    title = models.CharField(max_length = 300)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
