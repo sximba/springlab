4ea9dfd8d6ad120125777a60e0699c36f6dd9234
Sandile <lungeloximba@gmail.com>
2014-06-21 10:31:33 +0200

    Paginated video list
    -- Video list paginated to 10 result per page

--- ytv/views.py
+++ ytv/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_list_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
 from ytv.models import Video
@@ -6,7 +7,18 @@ from ytv.models import Video
 def index(request):
     if request.method == "GET":
         video_list = Video.objects.all()
-        context = {'video_list' : video_list}
+        paginator = Paginator(video_list, 10)
+        page = request.GET.get('page')
+
+        try:
+            videos = paginator.page(page)
+        except PageNotAnInteger:
+            # Deliver first page
+            videos = paginator.page(1)
+        except EmptyPage:
+            # Deliver last page
+            videos = paginator.page(paginator.num_pages)
+        context = {'video_list' : videos}
         return render(request, 'ytv/videos.html', context)
     elif request.method == "POST":
         return HttpResponse("You are posting a video")
