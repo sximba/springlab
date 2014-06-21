4ca518a7cb3d005adb2fee6b1b3181fcc3df4977
Sandile <lungeloximba@gmail.com>
2014-06-21 09:50:52 +0200

    Displayed minimal video list
    -- Initial video list displayed in template

--- ytv/views.py
+++ ytv/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
+from django.http import HttpResponse
 
-# Create your views here.
+from ytv.models import Video
+
+def index(request):
+    if request.method == "GET":
+        video_list = Video.objects.all()
+        context = {'video_list' : video_list}
+        return render(request, 'ytv/videos.html', context)
+    elif request.method == "POST":
+        return HttpResponse("You are posting a video")
+
+def destroy(request, video_id):
+    return HttpResponse("You are deleting video %s." % video_id)
