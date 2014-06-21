a1788126b17de2d754fbb6fc1e7e0b520c9572de
Sandile <lungeloximba@gmail.com>
2014-06-22 01:07:05 +0200

    Refactored the views
    -- Refactored the views
    -- Added the messages framework
    -- Redirected after successful post

---
+++ ytv/msgs.py
@@ -0,0 +1,4 @@
+em = "error_message"
+unreachable = "Can't seem to reach that video :( sure it exists?"
+no_title = "Video not interesting :( Has no title"
+no_descr = "Video has no description :( Not interesting to me"
--- ytv/views.py
+++ ytv/views.py
@@ -1,29 +1,38 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from django.utils import timezone
+from django.contrib import messages
 
-from ytv import youtube
+from ytv import youtube, msgs
 from ytv.models import Video
 
 def index(request):
-    context = {}
     if request.method == "POST":
         yturl = request.POST['yturl']
         details = youtube.get_details(yturl)
         if details == None:
-            context["error_message"] = "Can't seem to reach that video :( sure it exists?"
+            messages.error(request, msgs.unreachable)
         else:
             _ytid, _title, _descr = details
             if _title == None:
-                context["error_message"] = "Video not interesting :( Has no title"
+                messages.error(request, msgs.no_title)
             elif _descr == None:
-                context["error_message"] = "Video has no description :( Not interesting to me"
+                messages.error(request, msgs.no_descr)
             else:
                 pt = timezone.now()
                 v = Video(url=yturl, ytid=_ytid, title=_title, description=_descr, post_date=pt)
                 v.save()
+        return HttpResponseRedirect(reverse('ytv:index'))
+    else:
+        context = {"video_list":paginate(request)}
+        return render(request, 'ytv/videos.html', context)
 
+def destroy(request, video_id):
+    return HttpResponse("You are deleting video %s." % video_id)
+            
+def paginate(request):
     video_list = Video.objects.all().order_by('-post_date')
     paginator = Paginator(video_list, 10)
     page = request.GET.get('page')
@@ -36,9 +45,4 @@ def index(request):
     except EmptyPage:
         # Deliver last page
         videos = paginator.page(paginator.num_pages)
-    context["video_list"] = videos
-    return render(request, 'ytv/videos.html', context)
-            
-
-def destroy(request, video_id):
-    return HttpResponse("You are deleting video %s." % video_id)
+    return videos
